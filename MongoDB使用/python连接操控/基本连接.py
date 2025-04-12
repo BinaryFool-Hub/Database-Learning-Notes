413acc83b9fb519f4d0703487902a5dec000059d
BinaryFool <daishibing2006@gmail.com>
2025-04-12 19:02:23 +0800

    添加了账号密码连接

--- MongoDB使用/python连接操控/基本连接.py
+++ MongoDB使用/python连接操控/基本连接.py
@@ -2,7 +2,13 @@
 import pymongo
 
 # 连接MongoDB服务
-client = pymongo.MongoClient('192.168.171.128', 27017)
+# 没有刻意设置通常不需要账号密码
+client = pymongo.MongoClient(
+    host='192.168.171.128',
+    port=27017,
+    username="root",
+    password="root"
+)
 
 print(client)  # 输出对象信息
 
