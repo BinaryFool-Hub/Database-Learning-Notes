6d2fa321d3547a3cba50ce577329463c162929b5
BinaryFool <daishibing2006@gmail.com>
2025-04-16 16:19:33 +0800

    添加了删除和添加了多个键的检查约束

--- MySQL使用/语法、语句/复杂操作语句/字段检查约束.sql
+++ MySQL使用/语法、语句/复杂操作语句/字段检查约束.sql
@@ -2,11 +2,22 @@
 
 USE python_study;
 
--- --------------------------------- 表字段默认值一约束 ---------------------------------
+-- --------------------------------- 表字段检查约束 ---------------------------------
 -- 使用函数 CHECK() 来检查插入的数据是否满足条件，不能满足则报错
 CREATE TABLE tables_name
 (
     id       INT PRIMARY KEY AUTO_INCREMENT,
     name     VARCHAR(15),
-    password VARCHAR(15) CHECK ( LENGTH(password) > 6 AND LENGTH(password) < 20) -- 长度需要满足才能插入数据
+    password VARCHAR(15),
+    CONSTRAINT chk_password_length
+        CHECK (LENGTH(password) > 6 AND LENGTH(password) < 20) -- 长度需要满足才能插入数据
 );
+
+-- 删除检查约束
+-- 直接写入创建的约束名即可
+ALTER TABLE tables_name
+    DROP CONSTRAINT chk_password_length;
+
+-- 添加检查约束
+ALTER TABLE tables_name
+    ADD CONSTRAINT chk_password_length CHECK (LENGTH(password) > 6 AND LENGTH(password) < 20);
