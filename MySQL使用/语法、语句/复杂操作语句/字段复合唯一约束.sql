b8fdacbb3a5153f09ade771f82299e47194a288c
BinaryFool <daishibing2006@gmail.com>
2025-04-16 16:08:43 +0800

    添加了删除和添加了多个键的唯一约束

--- MySQL使用/语法、语句/复杂操作语句/字段复合唯一约束.sql
+++ MySQL使用/语法、语句/复杂操作语句/字段复合唯一约束.sql
@@ -13,3 +13,11 @@ CREATE TABLE tables_name
     CONSTRAINT uk_name_password UNIQUE (name, password),
     PRIMARY KEY (id)
 );
+
+-- 删除这个自动创建的同名索引，当使用关键字 UNIQUE 会创建一个同名索引
+ALTER TABLE tables_name
+    DROP INDEX uk_name_password;
+
+-- 添加唯一约束
+ALTER TABLE tables_name
+    ADD CONSTRAINT uk_name_password UNIQUE (name, password);
