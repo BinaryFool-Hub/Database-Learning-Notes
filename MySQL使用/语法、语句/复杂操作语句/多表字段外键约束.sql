ffb076c4b03bbd86fa63e7f765b6cdd34dbc29c9
BinaryFool <daishibing2006@gmail.com>
2025-04-16 15:39:01 +0800

    添加了 添加新的外键 删除外键 语句

--- MySQL使用/语法、语句/复杂操作语句/多表字段外键约束.sql
+++ MySQL使用/语法、语句/复杂操作语句/多表字段外键约束.sql
@@ -4,8 +4,8 @@ USE python_study;
 -- 然后依次查询需要的信息（id）即可
 -- 完全可以使用多个外键来继续进行关联操作，只不过多写一句外键语句而已 CONSTRAINT association_id FOREIGN KEY (author_id) REFERENCES author (id)
 
--- --------------------------------- 多表字段外键约束创建 ---------------------------------
--- 先创建作者的信息
+-- --------------------------------- 多表字段外键约束创建删除 ---------------------------------
+-- 先创建主表作者的信息
 CREATE TABLE author
 (
     id    INT PRIMARY KEY AUTO_INCREMENT,
@@ -14,7 +14,7 @@ CREATE TABLE author
     hobby varchar(20)
 );
 
--- 再创建作者的文章信息
+-- 再创建子表作者的文章信息
 -- 使用外键来关联另一张表
 -- association_id：自定义名称
 -- FOREIGN KEY (author_id): 用这个字段作为外键
@@ -27,6 +27,17 @@ CREATE TABLE article
     author_id INT,
     CONSTRAINT association_id FOREIGN KEY (author_id) REFERENCES author (id)
 );
+
+-- 删除外键
+ALTER TABLE article
+    DROP FOREIGN KEY association_id;
+
+-- 添加新的外键(如果创建表的时候没有外键可以修改)
+-- CONSTRAINT：约束，后面跟的是约束的字段
+-- FOREIGN KEY (author_id) REFERENCES author (id)：设置author_id为外键，并且author (id)表密码的字段id为约束
+ALTER TABLE article
+    ADD CONSTRAINT association_id
+        FOREIGN KEY (author_id) REFERENCES author (id);
 
 -- --------------------------------- 插入规则 ---------------------------------
 -- 插入的数据键必须在外键中已经存在了，不然会报错
