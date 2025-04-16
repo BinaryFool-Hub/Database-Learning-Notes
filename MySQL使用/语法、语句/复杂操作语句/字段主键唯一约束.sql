773954ee6b8ae3fc6d82cec682256a42cf24af3b
BinaryFool <daishibing2006@gmail.com>
2025-04-16 15:50:20 +0800

    添加了含有 AUTO_INCREMENT 属性删除示例

--- MySQL使用/语法、语句/复杂操作语句/字段主键唯一约束.sql
+++ MySQL使用/语法、语句/复杂操作语句/字段主键唯一约束.sql
@@ -9,11 +9,24 @@ USE python_study;
 -- AUTO_INCREMENT
 CREATE TABLE tables_name
 (
-    id       INT PRIMARY KEY,
+    id       INT PRIMARY KEY AUTO_INCREMENT,
     name     VARCHAR(15),
     password VARCHAR(15)
 );
+
+
+-- 如果有自增长AUTO_INCREMENT需要先删除
+-- 修改id列只有int属性,如果有其他属性还需要的需要加上，因为这是直接修改了id字段的全部属性
+ALTER TABLE tables_name
+    MODIFY id INT;
+
 -- 删除主键约束，不需要指定主键名，因为一个表只有一主键，删除主键约束后，非空约束还在
 -- 如果一个字段同时设置了主键和自增长，则不能删除主键约束（无法删除）
 ALTER TABLE tables_name
     DROP PRIMARY KEY;
+
+
+# 你不能删除主键的原因可能有以下几种情况：
+# 主键列有 AUTO_INCREMENT 属性：在 MySQL 中，如果一个列既是主键又有 AUTO_INCREMENT 属性，你不能直接删除这个主键约束。需要先移除 AUTO_INCREMENT 属性，然后再删除主键。
+# 外键约束：如果这个主键被其他表作为外键引用，你需要先删除那些外键约束。
+# 唯一索引问题：在某些 MySQL 版本中，主键会自动创建唯一索引，可能会有相关限制。
